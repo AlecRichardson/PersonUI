3f29866e0f1627ad6d7e7550f4d1e6c1f28cb6ea
Alec Richardson <alec@quarkworks.co>
2019-02-09 16:11:14 -0600

    Created person UI with constraint layout

---
+++ PersonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonUI
+//
+//  Created by Alec Richardson on 2/9/19.
+//  Copyright © 2019 Alec Richardson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
